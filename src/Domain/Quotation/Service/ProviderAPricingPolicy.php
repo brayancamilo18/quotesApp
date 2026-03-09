5f783356c7f2e2dc70b1b44b38bd454e69aaa323
brayancamilo18 <camilo189903@gmail.com>
2026-03-09 12:29:17 +0100

    politica de precios

---
+++ src/Domain/Quotation/Service/ProviderAPricingPolicy.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Domain\Quotation\Service;
+
+use App\Domain\Quotation\Model\Car;
+use App\Domain\Quotation\Model\QuoteRequest;
+use App\Domain\Shared\Money;
+
+final class ProviderAPricingPolicy
+{
+    private const BASE_PRICE = 217.0;
+
+    public function calculatePrice(QuoteRequest $request): Money
+    {
+        $driver = $request->getDriver();
+        $car = $request->getCar();
+
+        $price = self::BASE_PRICE;
+
+        // Edad
+        $age = $driver->getAge();
+        if ($age >= 18 && $age <= 24) {
+            $price += 70;
+        } else if ($age >= 56) {
+            $price += 90;
+        }
+
+        // Tipo de vehículo
+        switch ($car->getType()) {
+            case Car::TYPE_SUV;
+                $price += 100;
+                break;
+            case Car::TYPE_TURISMO:
+            case Car::TYPE_COMPACTO:
+                $price += 10;
+                break;
+        }
+
+        // Uso comercial
+        if($car->isCommercialUse()){
+            $price = $price * 1.15;
+        }
+
+        return new Money($price, 'EUR');
+    }
+}
---
+++ src/Domain/Quotation/Service/ProviderBPricingPolicy.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Domain\Quotation\Service;
+
+use App\Domain\Quotation\Model\Car;
+use App\Domain\Quotation\Model\QuoteRequest;
+use App\Domain\Shared\Money;
+
+final class ProviderBPricingPolicy
+{
+    private const BASE_PRICE = 250.0;
+
+    public function calculatePrice(QuoteRequest $request): Money
+    {
+        $driver = $request->getDriver();
+        $car = $request->getCar();
+
+        $price = self::BASE_PRICE;
+
+        // Edad
+        $age = $driver->getAge();
+        if ($age >= 18 && $age <= 29) {
+            $price += 50;
+        } elseif ($age >= 30 && $age <= 59) {
+            $price += 20;
+        } elseif ($age >= 60) {
+            $price += 100;
+        }
+
+        // Tipo de vehículo
+        switch ($car->getType()) {
+            case Car::TYPE_TURISMO:
+                $price += 30;
+                break;
+            case Car::TYPE_SUV:
+                $price += 200;
+                break;
+            case Car::TYPE_COMPACTO:
+                $price += 0;
+                break;
+        }
+
+        return new Money($price, 'EUR');
+    }
+}
